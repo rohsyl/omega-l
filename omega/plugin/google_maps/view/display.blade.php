2b85b94ad32cb671210d389a1e079cc7b5c1cd24
R-Men <huser.gaetan@hotmail.com>
2018-12-07 13:51:26 +0100

    GAPI call google_maps_ready(google) method to allow external usage of GAPI

--- omega/plugin/google_maps/view/display.blade.php
+++ omega/plugin/google_maps/view/display.blade.php
@@ -1,44 +1,47 @@
 @if(isset($apikey) && !empty($apikey))
     @if(isset($lat) && !empty($lat) && isset($long) && !empty($long))
-    <div class="plugin plugin-googlemaps">
-        <div id="{{ $uid }}_map" class="map" style="height: 400px;"></div>
-        <script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key={{ $apikey }}&callback=initMap" type="text/javascript"></script>
-        <script type="text/javascript">
-            function initMap() {
-                var latlng = new google.maps.LatLng({{ $lat }}, {{ $long }});
-                var myOptions = {
-                    zoom: 14,
-                    center: latlng,
-                    scrollwheel: false,
-                    scaleControl: false,
-                    disableDefaultUI: false,
-                    @if($mapstyleEnabled && !empty($mapstyle))
-                    styles: {!! $mapstyle !!} ,
+        <div class="plugin plugin-googlemaps">
+            <div id="{{ $uid }}_map" class="map" style="height: 400px;"></div>
+            <script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key={{ $apikey }}&libraries=places&callback=initMap" type="text/javascript"></script>
+            <script type="text/javascript">
+                function initMap() {
+                    var latlng = new google.maps.LatLng({{ $lat }}, {{ $long }});
+                    var myOptions = {
+                        zoom: 14,
+                        center: latlng,
+                        scrollwheel: false,
+                        scaleControl: false,
+                        disableDefaultUI: false,
+                        @if($mapstyleEnabled && !empty($mapstyle))
+                        styles: {!! $mapstyle !!} ,
+                        @endif
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    };
+                    var map = new google.maps.Map(document.getElementById("{{ $uid }}_map"),myOptions);
+
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: map.getCenter(),
+                        @if(isset($markerPicture))
+                        icon: '{{ $markerPicture->path }}',
+                        @endif
+                    });
+
+                            @if(isset($markerText) && !empty($markerText))
+                    var contentString = "{{ $markerText }}";
+                    var infowindow = new google.maps.InfoWindow({ content: contentString });
+
+                    google.maps.event.addListener(marker, "click", function() {
+                        infowindow.open(map,marker);
+                    });
                     @endif
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                };
-                var map = new google.maps.Map(document.getElementById("{{ $uid }}_map"),myOptions);
 
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: map.getCenter(),
-                    @if(isset($markerPicture))
-                    icon: '{{ $markerPicture->path }}',
-                    @endif
-                });
-
-                @if(isset($markerText) && !empty($markerText))
-                var contentString = "{{ $markerText }}";
-                var infowindow = new google.maps.InfoWindow({ content: contentString });
-
-                google.maps.event.addListener(marker, "click", function() {
-                    infowindow.open(map,marker);
-                });
-                @endif
-
-            }
-        </script>
-    </div>
+                    if (typeof google_maps_ready !== "undefined") {
+                        google_maps_ready(google);
+                    }
+                }
+            </script>
+        </div>
     @else
         <div class="plugin plugin-googlemaps">
             {{ __('Lat and/or lng undefined...') }}
