6bea5c08f057d1d8ac0cb50923edca56414d69f9
Sylvain Roh <syzin12@gmail.com>
2019-02-20 13:16:05 +0100

    Fixes #10

--- resources/views/pages/component/create.blade.php
+++ resources/views/pages/component/create.blade.php
@@ -1,19 +1,20 @@
 
 <div class="row">
     @if(count($plugins) == 0)
-    <p class="text-center">{{ __('No components available...') }}</p>
+        <p class="text-center">{{ __('No components available...') }}</p>
+        <p class="text-center text-info"><i class="fa fa-info"></i> {{ __('To add a component you can install plugins from the Plugins page in the left menu') }}</p>
     @else
-    <div style="max-height: 450px; overflow-y:auto;">
-        @foreach($plugins as $plugin)
-        @php $meta = new \Omega\Utils\Plugin\PluginMeta($plugin->name); @endphp
-        <div class="col-sm-4">
-            <div class="component-container" data-pluginid="{{ $plugin->id }}" >
-                <div class="component-image" style="background-image: url('{{ plugin_asset($plugin->name, 'images/component-logo.png') }}');"></div>
-                <div class="component-title">{{ $meta->getTitle() }}</div>
-            </div>
+        <div style="max-height: 450px; overflow-y:auto;">
+            @foreach($plugins as $plugin)
+            @php $meta = new \Omega\Utils\Plugin\PluginMeta($plugin->name); @endphp
+                <div class="col-sm-4">
+                    <div class="component-container" data-pluginid="{{ $plugin->id }}" >
+                        <div class="component-image" style="background-image: url('{{ plugin_asset($plugin->name, 'images/component-logo.png') }}');"></div>
+                        <div class="component-title">{{ $meta->getTitle() }}</div>
+                    </div>
+                </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>
     @endif
 </div>
 <script>
