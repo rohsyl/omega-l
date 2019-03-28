267d66764e4f2e9578b987c31bf9f3ffc4fb95b0
Sylvain Roh <syzin12@gmail.com>
2019-03-28 14:49:08 +0100

    Comment BController

--- app/Utils/Plugin/BController.php
+++ app/Utils/Plugin/BController.php
@@ -10,20 +10,49 @@ use Omega\Utils\Path;
 
 class BController extends AbstractController{
 
+    /**
+     * @var string The name of the plugin
+     */
 	protected $name;
+
+    /**
+     * @var int The id of the plugin
+     */
 	protected $id;
+
+    /**
+     * @var string The path to the plugin directory
+     */
 	protected $root;
+
+    /**
+     * @var PluginMeta MetaInformations about the plugin
+     */
 	private $meta;
+
+    /**
+     * @deprecated
+     * @var mixed Dont know what is this
+     */
 	public $model;
 
+    /**
+     * @var PluginRepository
+     */
 	private $pluginRepository;
 
+    /**
+     * @var null|integer The id of the current component. Null if we are not in a component context
+     */
 	public $idCurrentComponent = null;
 
+    /**
+     * BController constructor.
+     * @param $name string The name of the plugin
+     */
 	public function  __construct($name) {
-        //$this->html = new Html();
 		$this->name = $name;
-		$this->root = Path::Combine(plugin_path(), $this->name);
+		$this->root = plugin_path($this->name);
 		$this->meta = new PluginMeta($this->name);
 		$this->pluginRepository = new PluginRepository(new PluginModel());
 
@@ -31,38 +60,69 @@ class BController extends AbstractController{
 			$this->id = $this->getIdFromDatabase();
 	}
 
-	public static function getClassName($name)
-	{
-	    $nameF = camelize_plugin($name);
-		return 'OmegaPlugin\\' . $nameF . '\\BController' . $nameF;
-	}
-
+    /**
+     * Get the id of the plugin
+     *
+     * @return int The id of the plugin
+     */
 	public function getId(){
 	    return $this->id;
     }
 
+    /**
+     * Get the meta object to access metainformations of the plugin
+     *
+     * @return PluginMeta The meta object
+     */
 	public function getMeta() {
 		return $this->meta;
 	}
-	
+
+    /**
+     * Include a php file that is located in the plugin directory
+     *
+     * @deprecated
+     * @param $filePath string
+     */
 	public function includeFile($filePath)
 	{
 		include_once ($this->root . DS . $filePath);
 	}
-	
+
+    /**
+     * Return true if the plugin is installed
+     *
+     * @return bool
+     */
 	public function isInstalled() {
 		$plugin = $this->pluginRepository->getByName($this->name);
 		return $plugin != null;
-	}	
-	
+	}
+
+    /**
+     * This method can be overrided to execute some code after the installation of the plugin
+     *
+     * @return bool True if success
+     */
 	public function install() {
 	    return true;
 	}
-	
+
+    /**
+     * This method can be overrided to execute some code after the uninstallation of the plugin
+     *
+     * @return bool True if success
+     */
 	public function uninstall() {
 		return true;
 	}
-	
+
+    /**
+     * Return true if the plugin is enabled
+     *
+     * @deprecated
+     * @return bool
+     */
 	public function isEnabled() {
 		if($this->isInstalled()) {
             $plugin = $this->pluginRepository->get($this->id);
@@ -70,30 +130,38 @@ class BController extends AbstractController{
 		}
 		return false;
 	}
-	
+
+    /**
+     * Enable the plugin
+     *
+     * @deprecated
+     * @return bool
+     */
 	public function enable() {
 		if($this->isInstalled()) {
             return $this->pluginRepository->enable($this->id);
 		}
 		return true;
 	}
-	
+
+    /**
+     * Disable the plugin
+     *
+     * @deprecated
+     * @return bool
+     */
 	public function disable() {
 		if($this->isInstalled()) {
             return $this->pluginRepository->disable($this->id);
 		}
 		return true;
 	}
-	
-	protected function getAdminLink($action, $param = array()) {
-	    $param = array_merge($param, array(
-            'plugin' => $this->name,
-            'action' => $action
-        ));
-	    return Url::Action('plugin', 'run', $param);
 
-	}
-
+    /**
+     * Execute a SQL file
+     *
+     * @param $sqlFileAbsPath string The absolute path to the file
+     */
 	protected function runSql($sqlFileAbsPath) {
 
 		$sql = file($sqlFileAbsPath);
@@ -120,6 +188,8 @@ class BController extends AbstractController{
 	}
 
     /**
+     * Return a view
+     *
      * @param $name
      * @return \Illuminate\Contracts\View\View
      */
@@ -129,20 +199,44 @@ class BController extends AbstractController{
         ]);
     }
 
+    /**
+     * Return the view with the detail of the plugin
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     protected function meta_view(){
 	    return view('plugin.meta')->with([
             'meta' => $this->getMeta()
         ]);
     }
 
+
+    /**
+     * Return json
+     *
+     * @param $data array The data
+     * @return \Illuminate\Http\JsonResponse
+     */
     protected function json($data){
 	    return response()->json($data);
     }
 
+    /**
+     * Redirect to the given action of this plugin
+     *
+     * @param $action string The action
+     * @param array $param Get parameters
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     protected function redirect($action, $param = []){
 	    return redirect(route_plugin($this->name, $action, $param));
     }
 
+    /**
+     * Get the id of the plugin
+     *
+     * @return integer|null
+     */
 	private function getIdFromDatabase() {
         $plugin = $this->pluginRepository->getByName($this->name);
         return $plugin->id;
@@ -169,6 +263,19 @@ class BController extends AbstractController{
             'name' => $this->name, '--force' => true
         ]);
         return $exitCode;
+    }
+
+
+    /**
+     * Get the class name of the BController of the given plugin name
+     *
+     * @param $name string The plugin name
+     * @return string The full class name (with name space)
+     */
+    public static function getClassName($name)
+    {
+        $nameF = camelize_plugin($name);
+        return 'OmegaPlugin\\' . $nameF . '\\BController' . $nameF;
     }
 }
 
