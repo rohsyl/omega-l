ae7edab18f9c511f333462012efba9547889b537
Sylvain Roh <syzin12@gmail.com>
2019-05-09 11:51:46 +0200

    Updater view

--- resources/views/update/check.blade.php
+++ resources/views/update/check.blade.php
@@ -8,6 +8,10 @@
     <hr />
     <div id="update_notification">
     </div>
+    <br />
+    <pre id="output" style="max-height: 500px">
+
+    </pre>
 @endsection
 
 @push('scripts')
@@ -20,11 +24,34 @@
                 success: function(response) {
                     if(response != ''){
                         $('#update_notification').append('<strong>Update Available <span class="badge badge-pill badge-info">'+response+'</span></strong>');
-                        $('#update_notification').append('<a role="button" href="updater.update" class="btn btn-sm btn-info pull-right">Update Now</a>');
+                        $('#update_notification').append('<a role="button" href="javascript:update()" class="btn btn-sm btn-info pull-right">Update Now</a>');
                         $('#update_notification').show();
                     }
                 }
             });
         });
+
+
+        function update() {
+
+            xhr = new XMLHttpRequest();
+            xhr.open("GET", route('laraupdater.update'), true);
+            xhr.onprogress = function(e) {
+                $('#output').append(e.currentTarget.responseText);
+                scrollToBottom();
+            };
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4) {
+                    $('#output').append('Complete');
+                    scrollToBottom();
+                }
+            };
+            xhr.send();
+        }
+
+        function scrollToBottom() {
+            var objDiv = document.getElementById("output");
+            objDiv.scrollTop = objDiv.scrollHeight;
+        }
     </script>
 @endpush
