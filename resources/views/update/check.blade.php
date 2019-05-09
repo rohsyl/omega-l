2386879c4b9d3e715943f5ec4518d03878cba0bf
Sylvain Roh <syzin12@gmail.com>
2019-05-09 12:14:56 +0200

    Improve update gui

--- resources/views/update/check.blade.php
+++ resources/views/update/check.blade.php
@@ -6,10 +6,10 @@
     <p>Before performing an update, it's <strong>strongly</strong> recommended to backup your files and your database so you can easly restore your website back in case of error during the update.</p>
     <p>The updater automatically put your website in maintenance mode during the update.</p>
     <hr />
-    <div id="update_notification">
+    <div id="update_notification" style="display: none">
     </div>
     <br />
-    <pre id="output" style="max-height: 500px">
+    <pre id="output" style="display: none; max-height: 500px;">
 
     </pre>
 @endsection
@@ -17,16 +17,24 @@
 @push('scripts')
     <script>
         $(document).ready(function() {
+            $ouput = $('#output');
+            $alert = $('#update_notification');
             $.ajax({
                 type: 'GET',
                 url: route('laraupdater.check'),
                 async: false,
                 success: function(response) {
+                    $alert.empty();
+                    $ouput.hide();
                     if(response != ''){
-                        $('#update_notification').append('<strong>Update Available <span class="badge badge-pill badge-info">'+response+'</span></strong>');
-                        $('#update_notification').append('<a role="button" href="javascript:update()" class="btn btn-sm btn-info pull-right">Update Now</a>');
-                        $('#update_notification').show();
+                        $alert.append('<strong>Update Available <span class="badge badge-pill badge-info">'+response+'</span></strong>');
+                        $alert.append('<a role="button" href="javascript:update()" class="btn btn-sm btn-info pull-right">Update Now</a>');
                     }
+                    else {
+                        $alert.append('<strong>No Update Available</strong>');
+                        $ouput.hide();
+                    }
+                    $alert.show();
                 }
             });
         });
@@ -34,15 +42,17 @@
 
         function update() {
 
+            $ouput.show();
+
             xhr = new XMLHttpRequest();
             xhr.open("GET", route('laraupdater.update'), true);
             xhr.onprogress = function(e) {
-                $('#output').append(e.currentTarget.responseText);
+                $ouput.append(e.currentTarget.responseText);
                 scrollToBottom();
             };
             xhr.onreadystatechange = function() {
                 if (xhr.readyState == 4) {
-                    $('#output').append('Complete');
+                    $ouput.append('Complete');
                     scrollToBottom();
                 }
             };
