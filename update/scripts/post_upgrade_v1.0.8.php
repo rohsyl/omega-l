2981c14a36b9d1031c1181f543b12af0576eb7ae
Sylvain Roh <syzin12@gmail.com>
2019-05-09 14:35:23 +0200

    Update config in post_upgrade

--- update/scripts/post_upgrade_v1.0.8.php
+++ update/scripts/post_upgrade_v1.0.8.php
@@ -8,6 +8,11 @@ return OmegaUpgrader::toVersion('v1.0.8')->postUpgrade(function(ScriptResult $re
         $result->message('Start migrate');
         Artisan::call('migrate');
         $result->message('Migrate : [ OK ]');
+
+        $result->message('Update config');
+        config(['laraupdater.permissions.policy' => Omega\Utils\Upgrade\Policies\OmegaPermissionLaraUpdaterPolicy::class]);
+        config(['laraupdater.permissions.parameters.ability' => 'update_cms']);
+        $result->message('Config : [ OK ]');
     }
     catch(Exception $e) {
         $result->exception($e);
